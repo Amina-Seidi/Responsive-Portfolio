067292c6be49a6365aee7e8c072944d10702ba76
Minty <amina.seidi@uconn.edu>
2020-12-10 02:56:37 -0500

    deploy

---
+++ my-gatsby-project/.cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/aminaseidi/Documents/GitHub/Responsive-Portfolio-/my-gatsby-project/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": hot(preferDefault(require("/Users/aminaseidi/Documents/GitHub/Responsive-Portfolio-/my-gatsby-project/node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx")))
+}
+
